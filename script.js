2c2182c0c9edbf6d011f84d4a68f23859af5291a
Zibi95 <sylwester.sob95@gmail.com>
2022-09-28 11:57:07 +0200

    Game changer Add 5 rounds with for loop Add overall score function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const randomNumber = () => Math.floor(Math.random() * 3);
 const options = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
   const index = randomNumber();
   return options[index];
@@ -22,31 +23,63 @@ function playRound() {
   console.log(computerChoice);
   let playerChoice = getPlayerChoice();
 
+  let playerScore = 0,
+    computerScore = 0;
+
   if (playerChoice === "rock") {
-    if (computerChoice === "scissors")
+    if (computerChoice === "scissors") {
       console.log("You win! Rock beats scissors");
-    if (computerChoice === "paper")
+      playerScore++;
+    }
+    if (computerChoice === "paper") {
       console.log("You lost.... Rock loses with paper! Try next time");
+      computerScore++;
+    }
     if (computerChoice === "rock")
       console.log("It's a draw! You both picked rock!");
   }
 
   if (playerChoice === "paper") {
-    if (computerChoice === "rock") console.log("You win! Paper beats rock");
-    if (computerChoice === "scissors")
+    if (computerChoice === "rock") {
+      console.log("You win! Paper beats rock");
+      playerScore++;
+    }
+    if (computerChoice === "scissors") {
       console.log("You lost.... Rock loses with paper! Try next time");
+      computerScore++;
+    }
     if (computerChoice === "paper")
       console.log("It's a draw! You both picked paper!");
   }
 
   if (playerChoice === "scissors") {
-    if (computerChoice === "paper")
+    if (computerChoice === "paper") {
       console.log("You win! Scissors beats paper");
-    if (computerChoice === "rock")
+      playerScore++;
+    }
+    if (computerChoice === "rock") {
       console.log("You lost.... Scissors loses with rock! Try next time!");
+      computerScore++;
+    }
     if (computerChoice === "scissors")
       console.log("It's a draw! You both picked scissors!");
   }
+
+  return [playerScore, computerScore];
 }
 
-playRound();
+function results(score) {
+  if (score[0] > score[1]) console.log("You have won the match! GJ!");
+  if (score[0] < score[1])
+    console.log("Unfortunately computer was better this time :(");
+  if ((score[0] = score[1])) console.log("It's a draw");
+}
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+  playRound();
+  alert("Next round!");
+  if (i === 5) {
+    results(playRound());
+  }
+}
