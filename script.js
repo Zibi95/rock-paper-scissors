84c0c5270b96b6eb7287eacd8576eed9b813bb64
Zibi95 <sylwester.sob95@gmail.com>
2022-10-03 15:32:53 +0200

    Add ui add buttons and score

--- script.js
+++ script.js
@@ -1,93 +1,97 @@
-// RANDOM NUMBER GENERATOR FROM 0 TO 2
-const randomNumber = () => Math.floor(Math.random() * 3);
+let playerScore = 0,
+  computerScore = 0;
 
-// OPTIONS ARRAY
+// // OPTIONS ARRAY
 const options = ["rock", "paper", "scissors"];
 
-// FUNCTION - RETURNS COMPUTER CHOICE
+const buttons = document.querySelectorAll("button");
+const result = document.querySelector("h1");
+const roundResult = document.querySelector("h2");
+const playerResult = document.querySelector(".player-score");
+const computerResult = document.querySelector(".computer-score");
+// // RANDOM NUMBER GENERATOR FROM 0 TO 2
+const randomNumber = () => Math.floor(Math.random() * 3);
+
+// // FUNCTION - RETURNS COMPUTER CHOICE
 function getComputerChoice() {
   const index = randomNumber();
   return options[index];
 }
 
-//FUNCTION - PLAYER MAKE A CHOICE IN A PROMPT WINDOW, INFINITE LOOP IF INPUT IS MISSPELLED
-function getPlayerChoice() {
-  let choice = prompt(
-    "Choice your weapon. Rock? Paper? Scissors? Make a choice and win!"
-  ).toLowerCase();
-  while (1) {
-    if (options.includes(choice)) return choice;
-    choice = prompt(
-      "I think you misspelled something. Try again!"
-    ).toLowerCase();
-  }
-}
+// //FUNCTION - PLAYER MAKE A CHOICE IN A PROMPT WINDOW, INFINITE LOOP IF INPUT IS MISSPELLED
+// function getPlayerChoice() {
+//   let choice = prompt(
+//     "Choice your weapon. Rock? Paper? Scissors? Make a choice and win!"
+//   ).toLowerCase();
+// }
 
-// FUNCTION - DECIDES WHO WIN ROUND (LOTS OF IF), RETURNS ARRAY WITH OVERALL SCORE
-function playRound() {
+// // FUNCTION - DECIDES WHO WIN ROUND (LOTS OF IF), RETURNS ARRAY WITH OVERALL SCORE
+function playRound(e) {
   const computerChoice = getComputerChoice();
-  console.log(computerChoice);
-  let playerChoice = getPlayerChoice();
-
-  let playerScore = 0,
-    computerScore = 0;
+  let playerChoice = e.target.className;
 
   if (playerChoice === "rock") {
     if (computerChoice === "scissors") {
-      console.log("You win! Rock beats scissors");
+      roundResult.textContent = "You win! Rock beats scissors";
       playerScore++;
     }
     if (computerChoice === "paper") {
-      console.log("You lost.... Rock loses with paper! Try next time");
+      roundResult.textContent =
+        "You lost.... Rock loses with paper! Try next time";
       computerScore++;
     }
     if (computerChoice === "rock")
-      console.log("It's a draw! You both picked rock!");
+      roundResult.textContent = "It's a draw! You both picked rock!";
   }
 
   if (playerChoice === "paper") {
     if (computerChoice === "rock") {
-      console.log("You win! Paper beats rock");
+      roundResult.textContent = "You win! Paper beats rock";
       playerScore++;
     }
     if (computerChoice === "scissors") {
-      console.log("You lost.... Rock loses with paper! Try next time");
+      roundResult.textContent =
+        "You lost.... Rock loses with paper! Try next time";
       computerScore++;
     }
     if (computerChoice === "paper")
-      console.log("It's a draw! You both picked paper!");
+      roundResult.textContent = "It's a draw! You both picked paper!";
   }
 
   if (playerChoice === "scissors") {
     if (computerChoice === "paper") {
-      console.log("You win! Scissors beats paper");
+      roundResult.textContent = "You win! Scissors beats paper";
       playerScore++;
     }
     if (computerChoice === "rock") {
-      console.log("You lost.... Scissors loses with rock! Try next time!");
+      roundResult.textContent =
+        "You lost.... Scissors loses with rock! Try next time!";
       computerScore++;
     }
     if (computerChoice === "scissors")
-      console.log("It's a draw! You both picked scissors!");
+      roundResult.textContent = "It's a draw! You both picked scissors!";
   }
 
-  return [playerScore, computerScore];
-}
+  playerResult.lastChild.textContent = playerScore;
+  computerResult.lastChild.textContent = computerScore;
 
-//FUNCTION - DECIDES WHO WIN IN A GAME
-function results(score) {
-  if (score[0] > score[1]) console.log("You have won the match! GJ!");
-  if (score[0] < score[1])
-    console.log("Unfortunately computer was better this time :(");
-  if (score[0] === score[1]) console.log("It's a draw");
-}
-
-//LOOP - GAME HAS 5 ROUNDS
-for (let i = 1; i <= 5; ++i) {
-  console.log(i);
-  playRound();
-  alert("Next round!");
-  if (i === 5) {
-    results(playRound());
+  if (playerScore === 5 || computerScore === 5) {
+    gameEnd();
   }
 }
+
+// //FUNCTION - DECIDES WHO WIN IN A GAME
+function results(score1, score2) {
+  if (score1 > score2) result.textContent = "You have won the match! GJ!";
+  if (score1 < score2)
+    result.textContent = "Unfortunately computer was better this time :(";
+  if (score1 === score2) result.textContent = "It's a draw";
+}
+
+function gameEnd() {
+  roundResult.textContent = "";
+  buttons.forEach((button) => button.removeEventListener("click", playRound));
+  results(playerScore, computerScore);
+}
+
+buttons.forEach((button) => button.addEventListener("click", playRound));
